7f4a589b6d62a2beb0a0daa78e5251aedc2ca911
Erik S Carlsten <erik@preactiontechnology.com>
2013-04-02 15:51:49 -0600

    Added Joint class & testable BVH file

--- src/bvh.py
+++ src/bvh.py
@@ -2,4 +2,17 @@ __author__ = 'Erik S Carlsten'
 
 
 class BVH:
-    pass
+    def __init__(self, filename):
+        self.file = open(filename, 'r')
+
+    def _read_hierarchy(self):
+        pass
+
+
+class Joint:
+    def __init__(self, name, offset_x, offset_y, offset_z):
+        self.name = name
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
