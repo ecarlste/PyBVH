989e9290bffb18237eff75a81e43ef4a3013e4aa
Erik S Carlsten <erik@preactiontechnology.com>
2013-04-02 16:00:22 -0600

    Finished Joint constructor & added BVH._read_motion method

--- src/bvh.py
+++ src/bvh.py
@@ -8,10 +8,17 @@ class BVH:
     def _read_hierarchy(self):
         pass
 
+    def _read_motion(self):
+        pass
+
 
 class Joint:
-    def __init__(self, name, offset_x, offset_y, offset_z):
+    def __init__(self, name, offset_x, offset_y, offset_z, channels):
         self.name = name
+        self.offset.x = offset_x
+        self.offset.y = offset_y
+        self.offset.z = offset_z
+        self.channels = channels
         self.children = []
 
     def add_child(self, child):
